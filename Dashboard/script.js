d5d0cd3d4abb4ffb5a0223ca467c8833d5642f3a
Dev Reshamiya <reshamiyadev1234@gmail.com>
2024-08-31 15:05:34 +0530

    [WIP] Dashboard added stopwatch functionality

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -6,6 +6,7 @@ window.onload = async() => {
     quoteGen.loadQuote()
     let calenderObj = new calenderBlock()
     calenderObj.loadCalender()
+    let stopwatchObj = new stopwatchBlock()
 }
 
 class QuoteBlock{
@@ -55,4 +56,90 @@ class calenderBlock{
         document.getElementById("nextmonth").addEventListener('click', nextMonthCall)
         document.getElementById("prevmonth").addEventListener('click', prevMonthCall)
     }
+}
+
+class stopwatchBlock{
+    nodeSec = document.querySelector("#sec");
+    nodeMin = document.querySelector("#min");
+    nodeHour = document.querySelector("#hour");
+    lapsTable = document.querySelector(".stopwatchLapsBody");
+    laps = []
+    curSec = 0;
+    curHour = 0
+    curMin = 0;
+    curState = false;
+    interval = undefined;
+
+    constructor(){
+        let temp = new Date()
+        temp.setHours(0,0,0)
+        this.laps.push(temp)
+        document.getElementById('stopwatchPlay').addEventListener('click', ()=>{
+            this.play()
+        })
+        document.getElementById('stopwatchReset').addEventListener('click',()=>{
+            this.reset()
+        })
+        document.getElementById('stopwatchLap').addEventListener('click', ()=>{
+            this.lap()
+        })
+    }
+
+    play(){
+        if(!this.curState){
+            this.curState = true;
+            this.interval = setInterval(()=>{
+                if(this.curSec == 59){
+                    this.curMin = (this.curMin + 1) % 60
+                    this.nodeMin = this.curMin
+                    if(this.curMin == 59){
+                        this.curHour += 1
+                        this.nodeHour.innerHTML = this.curHour
+                    }
+                }
+                this.curSec = (this.curSec + 1) % 60;
+                this.nodeSec.innerHTML = this.curSec;
+            },1000)
+        }else{
+            this.curState = false;
+            clearInterval(this.interval);
+        }
+    }
+
+    reset(){
+        if(this.curState){
+            this.curState = false;
+            clearInterval(this.interval);
+        }
+        this.nodeHour.innerHTML = 0;
+        this.nodeMin.innerHTML = 0;
+        this.nodeSec.innerHTML = 0;
+    }
+
+    lap(){
+        let curDate = new Date();
+        curDate.setHours(this.curHour,this.curMin,this.curSec);
+        let lastDate = this.laps[this.laps.length-1];
+        let lapTime = Math.abs(curDate - lastDate);
+        const diffSeconds = Math.floor(lapTime / 1000);
+        const hours = Math.floor(diffSeconds / 3600);
+        const minutes = Math.floor((diffSeconds % 3600) / 60);
+        const seconds = diffSeconds % 60;
+        lapTime = new Date();
+        lapTime.setHours(hours,minutes,seconds);
+        this.laps.push(lapTime)
+        this.lapsTable.innerHTML += `
+        <tr>
+            <td class="p-1 colorBlue">
+                ${this.laps.length}
+            </td>
+            <td class="p-1">
+                ${lapTime.getHours()} : ${lapTime.getMinutes()} : ${lapTime.getSeconds()}
+            </td>
+            <td class="p-1">
+                ${this.curHour} : ${this.curMin} : ${this.curSec}
+            </td>
+        </tr>`
+    }
+
 }
--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,97 +1,106 @@
 let is_paused = true;
 let log = []
 
-function main(){
-    let t = window.setInterval(() => {
-        let cursec = document.getElementById("sec").innerHTML;
-        cursec = parseInt(cursec) + 1;
-        let curmin = document.getElementById("min");
-        let curhr = document.getElementById("hour");
+class stopwatch{
+    is_paused = true;
+    log = [];
+    
+    main(){
+        let t = window.setInterval(() => {
+            let cursec = document.getElementById("sec").innerHTML;
+            cursec = parseInt(cursec) + 1;
+            let curmin = document.getElementById("min");
+            let curhr = document.getElementById("hour");
+            if(! is_paused){
+                document.getElementById("sec").innerHTML = cursec;
+            }
+            if(cursec >= 60){
+                curmin.innerHTML = parseInt(curmin.innerHTML)+1
+                document.getElementById("sec").innerHTML = 0;
+            }
+            if(parseInt(curmin.innerHTML) >= 60){
+                curhr.innerHTML = parseInt(curhr.innerHTML) + 1;
+                curmin.innerHTML = 0
+            }
+    
+        }, 1000);
+    }    
+
+    pause(){
+        is_paused = is_paused? false : true;
+        document.getElementById("playbtn").innerHTML = is_paused? "Play":"Pause"
+    }
+
+    lap(){
         if(! is_paused){
-             document.getElementById("sec").innerHTML = cursec;
-        }
-        if(cursec >= 60){
-            curmin.innerHTML = parseInt(curmin.innerHTML)+1
-            document.getElementById("sec").innerHTML = 0;
-        }
-        if(parseInt(curmin.innerHTML) >= 60){
-            curhr.innerHTML = parseInt(curhr.innerHTML) + 1;
-            curmin.innerHTML = 0
-        }
-
-    }, 1000);
-}
-
-function pause(){
-
-    is_paused = is_paused? false : true;
-    document.getElementById("playbtn").innerHTML = is_paused? "Play":"Pause"
-}
-
-function lap(){
-    if(! is_paused){
-        let cursec = parseInt(document.getElementById("sec").innerHTML);
-        let curmin = parseInt(document.getElementById("min").innerHTML);
-        let curhr = parseInt(document.getElementById("hour").innerHTML);
-        log.push({
-            sec:cursec,
-            min:curmin,
-            hr:curhr
-        })
-        let secpassed = 0
-        let minpassed = 0
-        let hrpassed = 0
-        if (log.length>1){
-            if (log[log.length-2]["min"] < curmin){
-                secpassed = cursec + (60 - log[log.length-2]["sec"]);
-            }else{
-                secpassed = cursec - log[log.length-2]["sec"] ;
+            let cursec = parseInt(document.getElementById("sec").innerHTML);
+            let curmin = parseInt(document.getElementById("min").innerHTML);
+            let curhr = parseInt(document.getElementById("hour").innerHTML);
+            log.push({
+                sec:cursec,
+                min:curmin,
+                hr:curhr
+            })
+            let secpassed = 0
+            let minpassed = 0
+            let hrpassed = 0
+            if (log.length>1){
+                if (log[log.length-2]["min"] < curmin){
+                    secpassed = cursec + (60 - log[log.length-2]["sec"]);
+                }else{
+                    secpassed = cursec - log[log.length-2]["sec"] ;
+                }
+                if(log[log.length-2]["hr"] < curhr){
+                    minpassed = curmin + (60 - log[log.length-2]["min"]);
+                }else{
+                    minpassed = curmin - log[log.length-2]["min"];
+                }
+                hrpassed = curhr - log[log.length-2]["hr"];
+                if(secpassed > 60){
+                    minpassed += 1
+                    secpassed -= 60
+                }
+                if(minpassed > 60){
+                    minpassed -= 60
+                    hrpassed += 1
+                }
             }
-            if(log[log.length-2]["hr"] < curhr){
-                minpassed = curmin + (60 - log[log.length-2]["min"]);
-            }else{
-                minpassed = curmin - log[log.length-2]["min"];
+            let lap_time = log.length >1? `${hrpassed}:${minpassed}:${secpassed}`:`${curhr}:${curmin}:${cursec}`
+            const thead = `
+            <tr class="border-bottom border-white" id="laps">
+                <th>Lap</th>
+                <th>Lap Time</th>
+                <th>Overall Time</th>
+            </tr>`
+            const trow = `
+            <tr>
+                <th>${log.length-1}</th>
+                <td>${lap_time}</td>
+                <td>${curhr}:${curmin}:${cursec}</td>
+            </tr>`
+    
+            let lap = document.getElementById("laps");
+            if(log.length == 1){
+                lap.innerHTML += thead
             }
-            hrpassed = curhr - log[log.length-2]["hr"];
-            if(secpassed > 60){
-                minpassed += 1
-                secpassed -= 60
-            }
-            if(minpassed > 60){
-                minpassed -= 60
-                hrpassed += 1
-            }
+            lap.innerHTML+=trow;
         }
-        let lap_time = log.length >1? `${hrpassed}:${minpassed}:${secpassed}`:`${curhr}:${curmin}:${cursec}`
-        const thead = `
-        <tr class="border-bottom border-white" id="laps">
-            <th>Lap</th>
-            <th>Lap Time</th>
-            <th>Overall Time</th>
-        </tr>`
-        const trow = `
-        <tr>
-            <th>${log.length-1}</th>
-            <td>${lap_time}</td>
-            <td>${curhr}:${curmin}:${cursec}</td>
-        </tr>`
+    }
 
-        let lap = document.getElementById("laps");
-        if(log.length == 1){
-            lap.innerHTML += thead
-        }
-        lap.innerHTML+=trow;
+    reset(){
+        document.getElementById("sec").innerHTML = 0
+        document.getElementById("min").innerHTML = 0
+        document.getElementById('hour').innerHTML = 0
+        document.getElementById("laps").innerHTML = ""
+        log = []
+        pause()
     }
 }
 
-function reset(){
-    document.getElementById("sec").innerHTML = 0
-    document.getElementById("min").innerHTML = 0
-    document.getElementById('hour').innerHTML = 0
-    document.getElementById("laps").innerHTML = ""
-    log = []
-    pause()
-}
+let stopwatchObj = new stopwatch()
+window.onload = stopwatchObj.main()
 
-main()
-
+window.pause = stopwatchObj.pause;
+window.lap = stopwatchObj.lap;
+window.reset = stopwatchObj.reset;
+export default stopwatch;
